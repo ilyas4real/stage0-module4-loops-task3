ca7c346709c84dc3fa768eff732a865796588505
ilyas4real <iliyas.ysmaiyl@gmail.com>
2023-02-05 21:28:15 +0100

    done

--- src/main/java/school/mjc/stage0/loops/task3/GreatestCommonDivisor.java
+++ src/main/java/school/mjc/stage0/loops/task3/GreatestCommonDivisor.java
@@ -4,17 +4,19 @@ public class GreatestCommonDivisor {
     public void printGCD(int first, int second) {
         int gcd = 1;
         for(int i = 1; i <= first && i <= second; i++) {
-            if(first%i==0 && second%i==0) {
+            if (first % i == 0 && second % i == 0) {
                 gcd = i;
             }
-            if (first == 0) {
-                System.out.println(second);
-            } else if (second == 0) {
-                System.out.println(first);
-            } else {
-                System.out.println(gcd);
-            }
         }
+
+        if (first == 0) {
+            System.out.println(second);
+        } else if (second == 0) {
+            System.out.println(first);
+        } else {
+            System.out.println(gcd);
+        }
+
         System.out.println(gcd);
     }
 }
